edf87c2d08a63033eb7d54dcc9358da5446b0119
loneseu <lone.sh.seu@gmail.com>
2022-03-24 22:25:31 +0800

    gateway 转发

--- hermes-user/src/main/java/cn/edu/seu/lone/hermes/user/controller/HelloController.java
+++ hermes-user/src/main/java/cn/edu/seu/lone/hermes/user/controller/HelloController.java
@@ -1,13 +1,28 @@
 package cn.edu.seu.lone.hermes.user.controller;
 
+import cn.edu.seu.lone.entity.user.User;
+import cn.edu.seu.lone.entity.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class HelloController {
+
+    private final UserRepository userRepository;
+
 
     @RequestMapping("/welcome")
     public String welcome() {
-        return "welcome to hermes platform!";
+        return "10682 welcome to hermes platform!";
+    }
+
+
+    @RequestMapping("/name/{name}")
+    public User getUserByName(@PathVariable String name) {
+        return userRepository.findUserByName(name);
     }
 }
